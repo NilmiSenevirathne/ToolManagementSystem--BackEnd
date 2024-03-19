230f76840b8245667d2acd19746e0ebadfd97a21
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-03-20 01:22:47 +0530

    create layers in tools entity
    names are Tools, ToolsService, ToolsContrller, ToolsRepo,

---
+++ ToolManagementSystem/src/main/java/com/DilumBMK/ToolManagementSystem/repository/ToolsRepo.java
@@ -0,0 +1,7 @@
+package com.DilumBMK.ToolManagementSystem.repository;
+
+import com.DilumBMK.ToolManagementSystem.model.Tools;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToolsRepo extends JpaRepository<Tools,Long> {
+}
