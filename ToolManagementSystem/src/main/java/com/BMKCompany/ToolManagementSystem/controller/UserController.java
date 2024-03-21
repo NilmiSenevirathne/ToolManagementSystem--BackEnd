fa98f2ea0c1653260bace5af346e96cedac8596e
Nishadi738 <127076932+Nishadi738@users.noreply.github.com>
2024-03-21 23:04:15 +0530

    create post mapping method in Usercontroller class

--- ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/UserController.java
+++ ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.BMKCompany.ToolManagementSystem.controller;
 
-
+import com.BMKCompany.ToolManagementSystem.Exception.UserNotFoundException;
 import com.BMKCompany.ToolManagementSystem.model.User;
 import com.BMKCompany.ToolManagementSystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,9 +15,18 @@ public class UserController {
     private UserRepository userRepository;
 
 
-    @PostMapping("/adduser")
-    User addUser(@RequestBody User addUser)
-    {
-        return userRepository.save(addUser);
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+
+                    user.setUsername(newUser.getUsername());
+                    user.setFirstname(newUser.getFirstname());
+                    user.setLastname(newUser.getLastname());
+                    user.setContact(newUser.getContact());
+                    user.setNic(newUser.getNic());
+                    user.setRole(newUser.getRole());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
