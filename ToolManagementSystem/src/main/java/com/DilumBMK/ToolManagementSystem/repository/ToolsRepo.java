10b79a90d2dd3a513d88322f03ea37423ebf7fae
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2024-03-22 11:52:27 +0530

    Admin changes

--- ToolManagementSystem/src/main/java/com/DilumBMK/ToolManagementSystem/repository/ToolsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.DilumBMK.ToolManagementSystem.repository;
-
-import com.DilumBMK.ToolManagementSystem.model.Tools;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToolsRepo extends JpaRepository<Tools,Long> {
-}
