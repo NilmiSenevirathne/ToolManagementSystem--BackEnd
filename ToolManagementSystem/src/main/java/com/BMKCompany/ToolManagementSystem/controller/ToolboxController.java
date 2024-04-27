3398d6cf3823eb18912489495c0443614ae12bc0
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-04-27 23:08:45 +0530

    modified the ToolboxController class

--- ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/ToolboxController.java
+++ ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/ToolboxController.java
@@ -2,6 +2,7 @@ package com.BMKCompany.ToolManagementSystem.controller;
 
 import com.BMKCompany.ToolManagementSystem.model.ToolBox;
 import com.BMKCompany.ToolManagementSystem.repository.ToolboxRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("toolbox")
 public class ToolboxController {
 
+    @Autowired
     private ToolboxRepo toolboxRepo;
 
     //retrieve toolbox data from database
