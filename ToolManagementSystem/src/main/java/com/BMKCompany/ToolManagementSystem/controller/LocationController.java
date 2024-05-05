cdd1ab77d792ac3710794477dad794f54cdecb22
anusari <116504727+anusari@users.noreply.github.com>
2024-05-06 00:13:02 +0530

    Update LocationController.java

--- ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/LocationController.java
+++ ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/controller/LocationController.java
@@ -1,6 +1,9 @@
 package com.BMKCompany.ToolManagementSystem.controller;
+import com.BMKCompany.ToolManagementSystem.Exception.LocationNotFoundException;
+import com.BMKCompany.ToolManagementSystem.Exception.ProjectNotFoundException;
 import com.BMKCompany.ToolManagementSystem.model.Location;
 
+import com.BMKCompany.ToolManagementSystem.model.Project;
 import com.BMKCompany.ToolManagementSystem.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,21 +12,31 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class LocationController {
 
     @Autowired
     private LocationRepository locationRepository;
 
     @PostMapping("/location")
-    Location newlocation(@RequestBody Location newlocation){
-           return locationRepository.save(newlocation);
+    Location newlocation(@RequestBody Location newlocation) {
+        return locationRepository.save(newlocation);
 
     }
 
     @GetMapping("/locations")
-    List<Location> getAllLocations(){return locationRepository.findAll();}
+    List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
 
 
-
+    @PutMapping("/locations/{locationId}")
+    Location UpdateLocation(@RequestBody Location newLocation,@PathVariable String locationId){
+        return  locationRepository.findById(locationId)
+                .map(Location-> {
+                            Location.setLocationId(newLocation.getLocationId());
+                            Location.setLocationName(newLocation.getLocationName());
+                            return locationRepository.save(Location);
+                        }).orElseThrow(()-> new LocationNotFoundException(locationId));
+    }
 }
