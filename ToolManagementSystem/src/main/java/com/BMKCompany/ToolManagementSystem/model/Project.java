75057b11204363a5bc52fe40aefbda2c60ba54e3
anusari <116504727+anusari@users.noreply.github.com>
2024-05-16 09:42:17 +0530

    Update Project.java

--- ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/model/Project.java
+++ ToolManagementSystem/src/main/java/com/BMKCompany/ToolManagementSystem/model/Project.java
@@ -26,54 +26,30 @@ public class Project {
 
     private String date;
 
-    public String getProjectId() {
-        return projectId;
-    }
+    public String getProjectId() { return projectId;}
 
-    public void setProjectId(String projectId) {
-        this.projectId = projectId;
-    }
+    public void setProjectId(String projectId) { this.projectId = projectId;}
 
-    public String getProjectName() {
-        return ProjectName;
-    }
+    public String getProjectName() { return ProjectName;}
 
-    public void setProjectName(String projectName) {
-        ProjectName = projectName;
-    }
+    public void setProjectName(String projectName) { ProjectName = projectName; }
 
-    public String getDescription() {
-        return Description;
-    }
+    public String getDescription() { return Description;}
 
-    public void setDescription(String description) {
-        Description = description;
-    }
+    public void setDescription(String description) { Description = description;}
 
-    public String getSiteSupervisorID() {
-        return SiteSupervisorID;
-    }
+    public String getSiteSupervisorID() { return SiteSupervisorID; }
 
-    public void setSiteSupervisorID(String siteSupervisorID) {
-        SiteSupervisorID = siteSupervisorID;
-    }
+    public void setSiteSupervisorID(String siteSupervisorID) { SiteSupervisorID = siteSupervisorID;}
 
-    public String getSiteSupervisorName() {
-        return SiteSupervisorName;
-    }
+    public String getSiteSupervisorName() { return SiteSupervisorName;}
 
-    public void setSiteSupervisorName(String siteSupervisorName) {
-        SiteSupervisorName = siteSupervisorName;
-    }
+    public void setSiteSupervisorName(String siteSupervisorName) { SiteSupervisorName = siteSupervisorName;}
 
-    public String getLocationId() {
-        return locationId;
-    }
+    public String getLocationId() { return locationId; }
 
 
-    public void setLocationId(String locationId) {
-        this.locationId = locationId;
-    }
+    public void setLocationId(String locationId) { this.locationId = locationId;}
 
 
 
